d4fbb527dd33ab04273a848cac281db9c0130b1d
Kotaro126 <68246581+Kotaro126@users.noreply.github.com>
2020-11-11 13:24:25 +0900

    投稿編集削除機能の実装

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,17 +1,16 @@
 class DiariesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_diary, only: [:show, :edit, :destroy]
+  before_action :move_to_index, except: [:index, :show, :search]
 
-  #新規投稿フォームへ遷移するnewアクション
-  def new
-    @user_post = UserPost.new
-  end
-
-  #トップページに日記投稿内容を一覧表示する際のindexアクション
   def index
     @diaries = Diary.order('created_at DESC')
   end
 
-  #新規投稿フォームから投稿するcreateアクション
+  def new
+    @user_post = UserPost.new
+  end
+
   def create
     @user_post = UserPost.new(post_params)
     if @user_post.valid?
@@ -23,11 +22,52 @@ class DiariesController < ApplicationController
   end
   
   def show
-    @diary = Diary.find(params[:id])
+  end
+
+  def edit
+    redirect_to action: :index unless current_user.id == @diary.user_id
+  end
+
+  def update
+    diary = Diary.find(params[:id])
+    diary_params = params.permit(:image, :title, :content, :user_id)
+    if diary.update!(diary_params)
+      redirect_to diary_path, notice: 'Book was successfully updated.'
+    else
+      render :edit, notice: 'update was failed'
+    end
+  end
+
+  def destroy
+    if user_signed_in? && current_user.id == @diary.user_id
+      @diary.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  def search
+    @diaries = Diary.search(params[:keyword])
   end
 
   private
   def post_params
     params.require(:user_post).permit(:image, :title, :content, :word_name, :example, :diary_id, :user_id).merge(user_id: current_user.id)
+  end
+
+  def diary_params
+    params.require(:diary).permit(:image, :title, :content).merge(user_id: current_user.id)
+  end
+
+  def set_diary
+    @diary = Diary.find(params[:id])
+    @word = Word.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module ChatDiary
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.load_defaults 6.0
     config.time_zone = 'Asia/Tokyo'
     config.i18n.default_locale = :ja
